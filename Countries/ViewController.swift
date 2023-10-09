d7fb6f59ace1d27131b74e257deac248f70eb3f3
Ameer <ameerontheline@gmail.com>
2023-10-09 08:13:20 +0300

    Initial Commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Sayed Ameer Almosawi on 09/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
